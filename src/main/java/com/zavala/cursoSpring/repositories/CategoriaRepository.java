ad0175bfbbb75d7cf5b86b5ff3829f0091700b64
Ramiro Zavala <ramiroaz@gmail.com>
2021-08-13 13:23:36 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/zavala/cursoSpring/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.zavala.cursoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zavala.cursoSpring.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+	
+}
