caed73f8fe71f36bfd03a8a1d52a4e84fca19ddd
Ramiro Zavala <ramiroaz@gmail.com>
2021-08-17 13:09:44 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/zavala/cursoSpring/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.zavala.cursoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zavala.cursoSpring.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
