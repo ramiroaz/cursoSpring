e515d696a625cbf1563825702e3e462d3234bbc5
Ramiro Zavala <ramiroaz@gmail.com>
2021-08-24 12:07:06 -0300

    Validação de e-mail não repetido na inserção de Cliente

--- src/main/java/com/zavala/cursoSpring/repositories/ClienteRepository.java
+++ src/main/java/com/zavala/cursoSpring/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.zavala.cursoSpring.repositories;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.zavala.cursoSpring.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);  //o Spring JPA entende que é um método de busca no campo Email, na tabela cliente
+	
 }
--- src/main/java/com/zavala/cursoSpring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/zavala/cursoSpring/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zavala.cursoSpring.domain.Cliente;
 import com.zavala.cursoSpring.domain.enums.TipoCliente;
 import com.zavala.cursoSpring.dto.ClienteNewDTO;
+import com.zavala.cursoSpring.repositories.ClienteRepository;
 import com.zavala.cursoSpring.resources.exception.FieldMessage;
 import com.zavala.cursoSpring.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email","E-mail já está cadastrado"));
 		}
 		
 		for (FieldMessage e : list) {
