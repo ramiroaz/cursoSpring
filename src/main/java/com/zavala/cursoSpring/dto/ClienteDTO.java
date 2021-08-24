bcd2977b384b44d344dbbcf9eb5984f30faa423c
Ramiro Zavala <ramiroaz@gmail.com>
2021-08-24 13:20:12 -0300

    Validação de e-mail nçao repetido na atualização de Cliente

--- src/main/java/com/zavala/cursoSpring/dto/ClienteDTO.java
+++ src/main/java/com/zavala/cursoSpring/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.zavala.cursoSpring.domain.Cliente;
+import com.zavala.cursoSpring.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
