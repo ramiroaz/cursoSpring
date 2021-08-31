acda6033f9883ef6c028e707429cbf4ae28ea458
Ramiro Zavala <ramiroaz@gmail.com>
2021-08-31 18:49:21 -0300

    Criando profile de Desenvolvimento

---
+++ src/main/java/com/zavala/cursoSpring/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.zavala.cursoSpring.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.zavala.cursoSpring.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;  //estrategia de geração do banco de dados
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if ( !"create".equals(strategy)) {
+			return false;  //não faz nada no BD
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
