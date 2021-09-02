0b500e175ec314b03043bea957804448cc18b452
Ramiro Zavala <ramiroaz@gmail.com>
2021-09-02 13:56:54 -0300

    Implementando ToString de Pedido

--- src/main/java/com/zavala/cursoSpring/services/PedidoService.java
+++ src/main/java/com/zavala/cursoSpring/services/PedidoService.java
@@ -3,18 +3,18 @@ package com.zavala.cursoSpring.services;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zavala.cursoSpring.domain.ItemPedido;
 import com.zavala.cursoSpring.domain.PagamentoComBoleto;
 import com.zavala.cursoSpring.domain.Pedido;
-import com.zavala.cursoSpring.domain.Produto;
 import com.zavala.cursoSpring.domain.enums.EstadoPagamento;
 import com.zavala.cursoSpring.repositories.ItemPedidoRepository;
 import com.zavala.cursoSpring.repositories.PagamentoRepository;
 import com.zavala.cursoSpring.repositories.PedidoRepository;
-import com.zavala.cursoSpring.repositories.ProdutoRepository;
 import com.zavala.cursoSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,10 +30,13 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	ProdutoService produtoService;
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido find(Integer id) {
 		
@@ -43,9 +46,11 @@ public class PedidoService {
 		
 	}
 	
+	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));  //Ramiro
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		
@@ -59,11 +64,14 @@ public class PedidoService {
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.00);
 			//ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
-			Optional<Produto> p = produtoRepository.findById(ip.getProduto().getId());		//Ramiro
-		    ip.setPreco(p.get().getPreco());
+			//Optional<Produto> p = produtoRepository.findById(ip.getProduto().getId());		//Ramiro
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));						//Ramiro
+		    ip.setPreco(ip.getProduto().getPreco());
 		    ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());  //Ramiro
+		
+		System.out.println(obj);  //Teste imprime pedido. Automaticamente chama ao ToString
 		
 		return obj;
 	}
