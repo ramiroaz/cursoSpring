390edfefffbfcbc8151af7314e6a40772351fe70
Ramiro Zavala <ramiroaz@gmail.com>
2021-09-03 14:01:09 -0300

    Implementando SmtpService com servidor do Google

--- src/main/java/com/zavala/cursoSpring/services/DBService.java
+++ src/main/java/com/zavala/cursoSpring/services/DBService.java
@@ -117,7 +117,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "ramiroaz@hotmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Ap 303", "Jardim", "38220834", cli1 ,cid1);
